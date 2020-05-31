139972aa9b3702924e6047fa8743399360a55ada
Martin Michalski <martmichals@gmail.com>
2020-05-31 15:54:58 -0500

    Creation of secrets file and data folder

---
+++ acquisition_scripts/secrets.py
@@ -0,0 +1,10 @@
+""" 
+    This file stores all user-specific information for the proper execution of the program
+    Entries denoted with an asterisk, *, are required, and the program will not run w/o their initialization
+"""
+
+# Key for the News API ("https://newsapi.org/") *
+NEWS_API_KEY = ''
+
+# Key for the Stock Data API ("https://finnhub.io/") *
+FINNHUB_API_KEY = ''
